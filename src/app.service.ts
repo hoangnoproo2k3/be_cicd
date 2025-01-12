3efc11e0422efaf21fa53fec1780d87a17a657c0
hoangviet <hoangnv@cyberk.io>
2025-01-12 10:28:14 +0700

    Update greeting message in AppService to reflect new content

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, được là ngon này ';
+    return 'Hello, thử full luồng tách biệt';
   }
 }
